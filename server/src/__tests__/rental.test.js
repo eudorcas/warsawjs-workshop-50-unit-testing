49712cc0f7a00b4c74d52805302f21b89d14dd9a
Klaudia Butkiewicz <klaudia@codequest.com>
2020-11-22 18:28:57 +0100

    Add missing tests

--- server/src/__tests__/rental.test.js
+++ server/src/__tests__/rental.test.js
@@ -1,7 +1,19 @@
 const Rental = require('../rentals/rental');
+const RentalBuilder = require('../rentals/rental-builder');
 
 test('should change state to deposit paid', () => {
-    const rental = new Rental(11, 22, 1, 1000);
+    const rental = new RentalBuilder().rentBy(1).selectCar(1).depositAmount(1000).inState(Rental.RESERVED).build();
 
     rental.payDeposit();
-})
+
+    expect(rental.getState()).toBe(Rental.DEPOSIT_PAID);
+});
+
+test('should change state to take car', () => {
+    const rental = new RentalBuilder().buildPaid();
+
+    rental.takeCar();
+
+    expect(rental.getState()).toBe(Rental.CAR_IN_USE);
+});
+
